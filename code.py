22e8c9743609dde149795a5961feb7f78812a8a3
P SATHISH KUMAR <sathishp.railway@gmail.com>
2017-08-21 21:05:23 +0530

    code added

---
+++ code.py
@@ -0,0 +1,4 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup 
+
+my_url = 'https://www.newegg.com/global/in/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=graphics+card&N=-1&isNodeId=1'
